aaeaeed8b093abbc386f979fe4dd29c757b4de48
Mutai Mwiti <mutaimwiti40@gmail.com>
2021-03-04 16:44:38 +0300

    Add hook with simple test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = () => {
+import { useEffect } from 'react';
 
+const useActivity = ({
+  onActivity,
+  onInactivity,
+  inactivityTimeout = 2000,
+}) => {
+  let timeout;
+
+  const startTimeout = () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      onInactivity();
+    }, inactivityTimeout);
+  };
+
+  const handleMouseMove = () => {
+    onActivity();
+    startTimeout();
+  };
+
+  useEffect(() => {
+    startTimeout();
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      clearTimeout(timeout);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 };
+
+module.exports = useActivity;
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
-describe('useActivity', function () {
-  it('should', async () => {
+import { renderHook } from '@testing-library/react-hooks/dom';
 
+import useActivity from '../src';
+
+describe('useActivity', function () {
+  it('should not blow up', async () => {
+    const onActivity = jest.fn();
+    const onInactivity = jest.fn();
+
+    renderHook(() =>
+      useActivity({
+        onActivity,
+        onInactivity,
+      }),
+    );
   });
 });
